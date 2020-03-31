e64a69ddec4af4156fc168caab42d37d83643231
Jason Kempel <kempel.jason13@gmail.com>
2020-03-31 01:57:51 -0500

    writing java for button functionality

--- app/src/main/java/com/kempeljason13/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/kempeljason13/funfacts/FunFactsActivity.java
@@ -3,12 +3,32 @@ package com.kempeljason13.funfacts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+    // declare our view variables
+    private TextView factTextView;
+    private Button showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // assign the views from the layout file to the corresponding variables
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+
     }
 }
