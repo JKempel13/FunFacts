6fff7b8bbd30eda6126ff1f4efd6bdcc3cae8f23
Jason Kempel <kempel.jason13@gmail.com>
2020-03-31 19:44:09 -0500

    added 10 random facts to display on emulator

--- app/src/main/java/com/kempeljason13/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/kempeljason13/funfacts/FunFactsActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class FunFactsActivity extends AppCompatActivity {
     // declare our view variables
     private TextView factTextView;
@@ -24,8 +26,27 @@ public class FunFactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built."
+                };
+
+
                 // the button was clicked, so update the fact TextView with a bew fact
-                String fact = "Ostriches can run faster than horses!";
+                // randomly select a fact
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(10);
+                String fact = randomNumber + "";
+
+                // update screen with our new fact
                 factTextView.setText(fact);
             }
         };
